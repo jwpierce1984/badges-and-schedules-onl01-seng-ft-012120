ec56dd1a1da18a802dfcd441fd73a92e5c570e5e
Justin Pierce <jwpierce1984@gmail.com>
2020-01-31 17:02:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,8 @@ end
 def assign_rooms(attendees)
   attendees.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badges| puts badges}
+  assign_rooms(attendees).each {|rooms| puts rooms}
+end
